885ac4f41a8d91b4753232e4740f7d7210306ec8
Adel Senhadji <adelsenhadjii@gmail.com>
2020-10-24 00:41:20 +0200

    add simple user creation endpoint

--- server.js
+++ server.js
@@ -1,20 +1,29 @@
 require('dotenv').config();
 
 const express = require('express');
+
 const app = express();
 app.use(express.json());
 
 const db = require('./db');
-const user = require('./models/user');
+const User = require('./models/user');
 
 // dev
 console.clear();
-
 db.connect();
 
-// app.get('/', (req, res) => {
-//   return res.status(200).send('Send.io')
-// })
+app.get('/', (req, res) => res.send('hey'));
+
+app.post('/users/create', async (req, res) => {
+	const user = {
+		username: req.body.username,
+		firstName: req.body.firstName,
+		lastName: req.body.lastName,
+	};
+
+	await User.create(user);
+	return res.status(201).send();
+});
 
 // port config
 const port = process.env.PORT || 4000;
