cdc677addf4ef6ae6be03055234396f5f355614f
Adel Senhadji <adelsenhadjii@gmail.com>
2020-11-01 15:56:59 +0100

    Add user existence check on token validation

--- auth.js
+++ auth.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { isPresent, isNotPresent } = require('./db');
 const User = require('./models/user');
 
 router.use(express.json());
@@ -14,24 +15,31 @@ router.post('/login', async (req, res) => {
 	const username = req.body.username.toLowerCase();
 	const { password } = req.body;
 
-	const userExists = await User.findByPk(username);
-	if (userExists && await bcrypt.compare(password, userExists.password)) {
+	const user = await User.findByPk(username);
+	if (user && await bcrypt.compare(password, user.password)) {
 		const token = jwt.sign(username, process.env.TOKEN);
 		return res.status(200).send(token);
 	}
+
 	return res.status(400).send('Wrong username or/and password');
 });
 
 const authToken = (req, res, next) => {
 	const token = req.headers.authorization;
 
-	if (token == null) return res.status(401).send('Bad request : no token');
+	if (token == null) return res.status(401).send('No token');
 
-	jwt.verify(token, process.env.TOKEN, (err, id) => {
-		if (err) return res.status(403).send('Forbidden : invalid token');
-		req.username = id;
+	jwt.verify(token, process.env.TOKEN, async (err, username) => {
+		// if (err) return res.status(403).send('Invalid token');
+		if (err || await isNotPresent(User, username)) {
+			return res.status(403).send('Invalid token');
+		}
+		req.username = username;
 		next();
+		return 0;
 	});
+
+	return 0;
 };
 
 module.exports = {
