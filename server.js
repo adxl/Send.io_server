e39b49e99ad05e9bca5750bf4ce2ad04ae341584
Adel Senhadji <adelsenhadjii@gmail.com>
2020-11-08 19:06:52 +0100

    Fix socket connection

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const http = require('http');
+const socketio = require('socket.io');
 
 const { connect } = require('./db');
 
@@ -12,9 +14,11 @@ const { friendshipRouter } = require('./routes/friendshipRoutes');
 const { messagingRouter } = require('./routes/messagingRoutes');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.use(cors());
 app.use(express.json());
+app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
 
 /* Authentication routes */
 app.use(authRouter);
@@ -28,6 +32,22 @@ app.use(friendshipRouter);
 /* Messaging related routes */
 app.use('/conversations', messagingRouter);
 
+io.on('connect', (socket) => {
+	const { id } = socket.handshake.query;
+	console.log(`${id} connected`);
+	socket.join(id);
+
+	socket.on('send-message', (data) => {
+		const { sender } = data;
+		const { message } = data;
+
+		io.emit('receive-message', `${sender} said : ${message}`);
+		console.log(`${sender} said : ${message}`);
+	});
+
+	socket.on('disconnect', () => console.log(`${id} left`));
+});
+
 // init
 const port = process.env.PORT || 4000;
-app.listen(port, () => connect());
+server.listen(port, () => connect());
